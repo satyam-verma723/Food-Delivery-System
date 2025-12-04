cc91323212b383d1b3b99cc78c82d7dab632e408
Satyam Verma <149812431+satyam-verma723@users.noreply.github.com>
2025-12-04 19:58:51 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 const App = () => {
 
-  const url="https://food-del-backend-cdnh.onrender.com/"
+  const url="https://food-del-backend-cdnh.onrender.com"
 
   return (
     <div>
